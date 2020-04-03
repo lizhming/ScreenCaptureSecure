6ea14162f6fb38968b073a7cb7ba5b01f7181278
lizhming <flymaxelib@hotmail.com>
2020-04-03 13:50:19 +0900

    ios

--- ios/ScreenCaptureSecure.m
+++ ios/ScreenCaptureSecure.m
@@ -3,7 +3,7 @@
 
 @implementation ScreenCaptureSecure
 
-id<NSObject> screenCaptureObserver;
+static id<NSObject> screenCaptureObserver;
 
 RCT_EXPORT_MODULE()
 
@@ -15,13 +15,14 @@ RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnu
 RCT_EXPORT_METHOD(enableSecure)
 {
     NSOperationQueue *mainQueue = [NSOperationQueue mainQueue];
+    if (screenCaptureObserver == nil) {
     screenCaptureObserver = [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationUserDidTakeScreenshotNotification
                                                     object:nil
                                                     queue:mainQueue
                                                 usingBlock:^(NSNotification *note) {
                                                     // executes after screenshot
                                                     UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"주의"
-                                                                                message:@"작품을 캡쳐한 스크린샷을 온라인, 오프라인에 무단으로 공유 및 배포할 경우 법적인 제재를 받을 수 있습니다."
+                                                                                message:@"작품을 캡쳐한 스크린샷을\n온라인, 오프라인에 무단으로\n공유 및 배포할 경우 법적인\n제재를 받을 수 있습니다."
                                                                                 preferredStyle:UIAlertControllerStyleAlert];
 
                                                     UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"확인" style:UIAlertActionStyleDefault
@@ -40,6 +41,7 @@ RCT_EXPORT_METHOD(enableSecure)
 
                                                     [rootViewController presentViewController:alert animated:YES completion:nil];
                                                 }];
+    }
 }
 
 RCT_EXPORT_METHOD(disableSecure)
